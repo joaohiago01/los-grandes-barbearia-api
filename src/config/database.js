d3b89c80bcdd15841c9d756232dcdd45702578c9
[hiago] <[joaohiago96@gmail.com]>
2020-03-01 18:53:09 -0300

    Ajustando configurações do postgresql no heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    host: 'localhost',
-    database: 'LosGrandesBarbearia',
-    username: 'postgres',
-    password: 'ads',
+    host: 'ec2-54-197-34-207.compute-1.amazonaws.com',
+    database: 'de23hn6llaqej9',
+    username: 'ynbsgadzkfsgec',
+    password: 'a31d663ed191261345094e2dc83b82111beb820f318d3c05e8c12bfb1b67f095',
     port: 5432,
     dialect: 'postgres',
     logging: console.log(),
